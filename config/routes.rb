5ebfd3a781ec4b6f5cda4af1f8545b90d50e5e98
Patricia Zegarra <patty.zegarrab@gmail.com>
2020-11-16 17:30:55 +0100

    models and controllers created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :pets, only: [:index, :show, :create, :new] do
+    resources :appointments, only: [:show, :create, :new]
+  end
+  resources :appointments, only: [:index, :destroy]
 end
