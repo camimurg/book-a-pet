269a9ee84bdf81f99a895b11d916b4d9a8591442
Patricia Zegarra <patty.zegarrab@gmail.com>
2020-11-17 16:06:58 +0100

    changes

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,19 +6,20 @@ class PetsController < ApplicationController
   def show
     @pet = Pet.find(params[:id])
   end
+
+  def new
+    @pet = Pet.new
+  end
+
+  def create
+    @pet = Pet.new(pet_params)
+    @pet.save
+    redirect_to pet_path(@pet)
+  end
+
+  private
+
+  def pet_params
+    params.require(:pet).permit(:name, :species, :address, :city, :description)
+  end
 end
-  # def new
-  #   @pet = Pet.new
-  # end
-
-  # def create
-  #   @pet = Pet.new(pet_params)
-  #   @pet.save
-  #   redirect_to pet_path(@pet)
-  # end
-
-  # private
-
-  # def pet_params
-  #   params.require(:pet).permit(:name, :species, :address, :city, :description)
-  # end
