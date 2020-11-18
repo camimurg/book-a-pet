c3e354dc7bf3144871ec5bf75b9583189b4a84de
Patricia Zegarra <patty.zegarrab@gmail.com>
2020-11-18 16:37:32 +0100

    pictures and seeds

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,12 +1,11 @@
 class Pet < ApplicationRecord
   has_many :appointments, dependent: :destroy
   belongs_to :user
-  SPECIES = ["Dog", "Cat", "Fish", "Hamster", "Bird", "Snake", "Frog"]
+  SPECIES = ["Dog", "Cat", "Fish", "Hamster", "Bird", "Snake", "Frog", "Turtle"]
   has_one_attached :picture
   validates :name, presence: true
   validates :species, presence: true, inclusion: { in: SPECIES }
   validates :address, presence: true
   validates :city, presence: true
   validates :description, presence: true
-  validates :picture, presence: true
 end
