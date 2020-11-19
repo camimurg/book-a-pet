593e7e5dfb4463a8413c3ed1c90054a835cc00c9
jose ferreira <zejulio3@hotmail.com>
2020-11-19 16:36:43 +0100

    migrated seeds

--- db/schema.rb
+++ db/schema.rb
@@ -58,7 +58,7 @@ ActiveRecord::Schema.define(version: 2020_11_18_105403) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "image"
+    t.string "picture"
     t.index ["user_id"], name: "index_pets_on_user_id"
   end
 
